02a3e2c0e560fcf5f1465c51248f7709ba447c55
Leonardo Weiss <lsweissleo@gmail.com>
2022-07-12 21:27:34 -0300

    player music's project

---
+++ script.js
@@ -0,0 +1,19 @@
+const musics = [
+    {
+        'name': 'Sorrisos feat. Lourena',
+        'artist': 'L7NNON',
+        'img': 'img/sorrisos.jpg',
+        'songPath': 'song/sorrisos.mp3'
+    },{
+        'name': 'Leal',
+        'artist': 'Djonga',
+        'img': 'img/leal.jpg',
+        'songPath': 'song/leal.mp3'
+    },{
+        'name': 'Lose Yourself',
+        'artist': 'Eminem',
+        'img': 'img/lose.jpg',
+        'songPath': 'song/lose.mp3'
+    }
+];
+
